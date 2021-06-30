885f8fb032c25f3dde7003e8089c0b03dfa67324
smerton <simonmerton@hotmail.co.uk>
2021-06-30 14:43:42 +0100

    finite-difference branch: Update bulk viscosity calculation with a more robust divergence term. This brings the finite difference variant up to date with the finite element variant.

--- main.cpp
+++ main.cpp
@@ -44,10 +44,10 @@ int main(){
 
   ofstream f1,f2,f3,f4;                                 // files for output
   int const n(100),ng(n+4);                             // no. ncells, no. ghosts
-  double const cl(0.0),cq(4.0);                         // linear & quadratic coefficients for bulk viscosity
-  vector<double> d(ng),p(ng),q(ng),V0(ng),V1(ng),m(ng); // pressure, bulk viscosity, density, volume & mass
+  double const cl(0.3),cq(1.0);                         // linear & quadratic coefficients for bulk viscosity
+  vector<double> d0(ng),d1(ng),V0(ng),V1(ng),m(ng);     // density, volume & mass
   vector<double> e0(ng),e1(ng);                         // cell-centred energy field
-  vector<double> c(ng);                                 // element sound speed
+  vector<double> c(ng),p(ng),q(ng);                     // element sound speed, pressure and bulk viscosity
   vector<double> u0(ng+1),u1(ng+1),utmp(ng+1);          // node velocity
   vector<double> x0(ng+1),x1(ng+1);                     // node coordinates
   vector<double> dt_cfl(ng);                            // element time-step
@@ -63,17 +63,18 @@ int main(){
   double dx(1.0/n);x0.at(0)=-2.0*dx;x1.at(0)=x0[0];
   for(int i=1;i<ng+1;i++){x0.at(i)=x0[i-1]+dx;x1.at(i)=x0[i];}
   for(int i=0;i<ng;i++){p.at(i)=(0.5*(x0[i]+x0[i+1])<=0.5)?l[2]:r[2];}
-  for(int i=0;i<ng;i++){d.at(i)=(0.5*(x0[i]+x0[i+1])<=0.5)?l[0]:r[0];}
-  for(int i=0;i<ng;i++){e0.at(i)=E(d[i],p[i]);}
-  for(int i=0;i<ng;i++){e1.at(i)=e0[i];}
+  for(int i=0;i<ng;i++){d0.at(i)=(0.5*(x0[i]+x0[i+1])<=0.5)?l[0]:r[0];}
+  for(int i=0;i<ng;i++){d1.at(i)=(0.5*(x1[i]+x1[i+1])<=0.5)?l[0]:r[0];}
+  for(int i=0;i<ng;i++){e0.at(i)=E(d0[i],p[i]);}
+  for(int i=0;i<ng;i++){e1.at(i)=E(d1[i],p[i]);}
   for(int i=0;i<ng;i++){V0.at(i)=x0[i+1]-x0[i];}
   for(int i=0;i<ng;i++){V1.at(i)=x1[i+1]-x1[i];}
-  for(int i=0;i<ng;i++){m.at(i)=d[i]*V0[i];}
+  for(int i=0;i<ng;i++){m.at(i)=d0[i]*V0[i];}
   for(int i=0;i<ng+1;i++){u0.at(i)=(x0[i]<=0.5)?l[1]:r[1];}
   for(int i=0;i<ng+1;i++){u1.at(i)=u0[i];}
   for(int i=0;i<ng+1;i++){utmp.at(i)=u0[i];}
   for(int i=0;i<ng;i++){q.at(i)=0.0;}
-  for(int i=0;i<ng;i++){c.at(i)=sqrt(GAMMA*p[i]/d[i]);}
+  for(int i=0;i<ng;i++){c.at(i)=sqrt(GAMMA*p[i]/d0[i]);}
   for(int i=2;i<ng-1;i++){ke+=0.25*(m[i-1]+m[i])*u0[i]*u0[i];}
   for(int i=2;i<ng-2;i++){ie+=e1[i]*m[i];}
 
@@ -91,7 +92,7 @@ int main(){
 
 // calculate a new stable time-step
 
-    for(int i=0;i<ng;i++){double l(x0[i+1]-x0[i]);dt_cfl.at(i)=(COURANT*l/sqrt((c[i]*c[i])+2.0*q[i]/d[i]));} // impose the CFL limit on each element
+    for(int i=0;i<ng;i++){double l(x0[i+1]-x0[i]);dt_cfl.at(i)=(COURANT*l/sqrt((c[i]*c[i])+2.0*q[i]/d0[i]));} // impose the CFL limit on each element
     double dt=DTSFACTOR*(*min_element(dt_cfl.begin(), dt_cfl.end())); // reduce across element and apply a saftey factor
 
     cout<<fixed<<setprecision(5)<<"  step "<<step<<" time= "<<time<<" dt= "<<dt<<" energy (i/k/tot)= "<<ie<<" "<<ke<<" "<<ie+ke<<endl;
@@ -119,7 +120,7 @@ int main(){
 
 // update cell density at the full-step
 
-    for(int i=0;i<ng;i++){d.at(i)=m[i]/V1[i];}
+    for(int i=0;i<ng;i++){d1.at(i)=m[i]/V1[i];}
 
 // debug
 //    for(int i=0;i<ng;i++){d.at(i)=R1.density(3*i+1);} // 3*i+1 is cell-centre address
@@ -139,15 +140,15 @@ int main(){
 
 // update cell pressure at the full-step
 
-    for(int i=0;i<ng;i++){p.at(i)=P(d[i],e1[i]);if(p[i]<0.0){cout<<"-'ve pressure detected in cell "<<i<<" e1= "<<e1[i]<<endl;exit(1);}}
+    for(int i=0;i<ng;i++){p.at(i)=P(d1[i],e1[i]);if(p[i]<0.0){cout<<"-'ve pressure detected in cell "<<i<<" e1= "<<e1[i]<<endl;exit(1);}}
 
 // bulk q
 
     for(int i=0;i<ng;i++){
-      c.at(i)=sqrt(GAMMA*p[i]/d[i]);
-      double dx(x1[i+1]-x1[i]),l(dx),gradu((u1[i+1]-u1[i])/dx),div(V1[i]-V0[i]);
-      if(div<0.0){
-        q.at(i)=(cq*d[i]*pow(l*gradu,2))-(cl*d[i]*c[i]*l*abs(gradu));
+      c.at(i)=sqrt(GAMMA*p[i]/d1[i]);
+      double l(x1[i+1]-x1[i]),divu((d0[i]-d1[i])/(d1[i]*dt));
+      if(divu<0.0){
+        q.at(i)=d0[i]*l*divu*((cq*l*divu)-cl*c[i]);
       }else{
         q.at(i)=0.0; // turn off q as cell divergence indicates expansion
       }
@@ -157,7 +158,7 @@ int main(){
 
     for(int i=1;i<ng;i++){
 
-      double dxl(x1[i]-x1[i-1]),dxr(x1[i+1]-x1[i]),dl(d[i-1]),dr(d[i]),pl(p[i-1]+q[i-1]),pr(p[i]+q[i]);
+      double dxl(x1[i]-x1[i-1]),dxr(x1[i+1]-x1[i]),dl(d1[i-1]),dr(d1[i]),pl(p[i-1]+q[i-1]),pr(p[i]+q[i]);
       double udot((pl-pr)/(0.5*((dl*dxl)+(dr*dxr))));
 
 // advance the solution
@@ -176,7 +177,7 @@ int main(){
     f1<<fixed<<setprecision(17);f2<<fixed<<setprecision(17);f3<<fixed<<setprecision(17);f4<<fixed<<setprecision(17);
 
     for(int i=0;i<NSAMPLES;i++){f1<<r0x[i]<<" "<<R0.density(i)<<" "<<R0.pressure(i)<<" "<<R0.velocity(i)<<" "<<R0.energy(i)<<endl;}
-    for(int i=2;i<n+2;i++){f2<<0.5*(x1[i]+x1[i+1])<<" "<<d[i]<<" "<<p[i]<<" "<<e1[i]<<" "<<endl;}
+    for(int i=2;i<n+2;i++){f2<<0.5*(x1[i]+x1[i+1])<<" "<<d1[i]<<" "<<p[i]<<" "<<e1[i]<<" "<<endl;}
     for(int i=2;i<n+2;i++){f3<<0.5*(x1[i]+x1[i+1])<<" "<<q[i]<<endl;}
     for(int i=2;i<n+3;i++){f4<<x1[i]<<" "<<u1[i]<<endl;}
 
@@ -197,7 +198,8 @@ int main(){
     for(int i=0;i<ng+1;i++){x0.at(i)=x1[i];}
     for(int i=0;i<ng;i++){e0.at(i)=e1[i];}
     for(int i=0;i<ng;i++){V0.at(i)=V1[i];}
-//    for(int i=0;i<ng;i++){c.at(i)=sqrt(GAMMA*(p[i]+q[i])/d[i]);}
+    for(int i=0;i<ng;i++){d0.at(i)=d1[i];}
+//    for(int i=0;i<ng;i++){c.at(i)=sqrt(GAMMA*(p[i]+q[i])/d1[i]);}
 
   }
 
