c288f373c782f2cdcf78dd80ac1eaa26d7ee279e
smerton <simonmerton@hotmail.co.uk>
2021-09-02 01:19:29 +0100

    Bug fix to force matrix. Add separate density fields and determinants for thermodynamics and kinematics. Energy field still too noisy, but it is looking better.

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@
 
 // Author S. R. Merton
 
-#define DTSTART 0.0005          // insert a macro for the first time step
+#define DTSTART 0.0001          // insert a macro for the first time step
 #define ENDTIME 0.25            // insert a macro for the end time
 #define GAMMA 1.4               // ratio of specific heats for ideal gases
 #define ECUT 1.0e-8             // cut-off on the energy field
@@ -72,22 +72,25 @@ int main(){
 // global data
 
   ofstream f1,f2,f3,f4,f5;                              // files for output
-  Shape K(4,10),T(3,10);                                // p_n,p_n-1 shape functions
+  Shape K(2,10),T(1,10);                                // p_n,p_n-1 shape functions
   int const n(40),ng(n+4);                              // no. ncells, no. ghosts
   int long nk(n*(K.nloc()-1)+1),nkg(ng*(K.nloc()-1)+1); // no. kinematic nodes, no. kinematic ghosts
   int long nt(n*T.nloc()),ntg(ng*T.nloc());             // no. thermodynamic nodes, no. thermodynamic ghosts
   double const cl(0.3),cq(1.0);                         // linear & quadratic coefficients for bulk viscosity
   vector<double> dinit(ng);                             // initial density field inside an element
-  vector<double> d0(ng*T.ngi()),d1(ng*T.ngi());         // density at each Gauss point in each element
+  vector<double> d0_t(ng*T.ngi()),d1_t(ng*T.ngi());     // density at each Gauss point in each element
+  vector<double> d0_k(ng*T.ngi()),d1_k(ng*T.ngi());     // density at each Gauss point in each element
   vector<double> V0(ng),V1(ng),m(ng),xc(ng);            // volume, mass & centroid
-  vector<double> nodmass(ng*T.nloc());                  // nodal mass
+  vector<double> nodmass_t(ntg),nodmass_k(nkg);         // nodal mass
+  vector<double> nodvol_t(ntg),nodvol_k(nkg);           // nodal volume
   vector<double> e0(ntg),e1(ntg);                       // discontinuous FE energy field
   vector<double> c(ng*T.ngi()),p(ng*T.ngi());           // element sound speed & pressure at each Gauss point
   vector<double> q(ng*T.ngi());                         // bulk viscosity at each Gauss point
   vector<double> u0(nkg),u1(nkg);                       // node velocity
   vector<double> x0(nkg),x1(nkg),x2(ntg),x3(ntg);       // node coordinates
   vector<double> dt_cfl(ng*T.ngi());                    // element time-step at each Gauss point
-  vector<double> detJ0(ng*T.ngi()),detJ(ng*T.ngi());    // determinant of the Jacobian
+  vector<double> detJ0_t(ng*T.ngi()),detJ_t(ng*T.ngi());    // determinant of the Jacobian
+  vector<double> detJ0_k(ng*K.ngi()),detJ_k(ng*K.ngi());    // determinant of the Jacobian
   double F[nkg][ntg],FT[ntg][nkg];                      // force matrix
   double ke(0.0),ie(0.0);                               // kinetic and internal energy for conservation checks
   double time(0.0),dt(DTSTART);                         // start time and time step
@@ -102,9 +105,11 @@ int main(){
   for(long i=0;i<nkg;i++){x0.at(i)=x0[0]+i*dx/(K.nloc()-1);x1.at(i)=x0[i];}
   for(long i=0;i<ntg;i++){x2.at(i)=x2[0]+i*dx/(T.nloc()-1);x3.at(i)=x2[i];}
   for(int i=0;i<ng;i++){xc.at(i)=CENTROID;}
-  for(int i=0;i<ng;i++){for(int gi=0;gi<T.ngi();gi++){p.at(GPNT)=(xc[i]<=0.5)?l[2]:r[2];}}
-  for(int i=0;i<ng;i++){for(int gi=0;gi<T.ngi();gi++){d0.at(GPNT)=(xc[i]<=0.5)?l[0]:r[0];}}
-  for(int i=0;i<ng;i++){for(int gi=0;gi<T.ngi();gi++){d1.at(GPNT)=(xc[i]<=0.5)?l[0]:r[0];}}
+  for(int i=0;i<ng;i++){for(int gi=0;gi<K.ngi();gi++){p.at(GPNT)=(xc[i]<=0.5)?l[2]:r[2];}}
+  for(int i=0;i<ng;i++){for(int gi=0;gi<T.ngi();gi++){d0_t.at(GPNT)=(xc[i]<=0.5)?l[0]:r[0];}}
+  for(int i=0;i<ng;i++){for(int gi=0;gi<K.ngi();gi++){d0_k.at(GPNT)=(xc[i]<=0.5)?l[0]:r[0];}}
+  for(int i=0;i<ng;i++){for(int gi=0;gi<T.ngi();gi++){d1_t.at(GPNT)=(xc[i]<=0.5)?l[0]:r[0];}}
+  for(int i=0;i<ng;i++){for(int gi=0;gi<K.ngi();gi++){d1_k.at(GPNT)=(xc[i]<=0.5)?l[0]:r[0];}}
   for(int i=0;i<ng;i++){for(int j=0;j<T.nloc();j++){e0.at(TNOD)=(xc[i]<=0.5)?E(l[0],l[2]):E(r[0],r[2]);}}
   for(int i=0;i<ng;i++){for(int j=0;j<T.nloc();j++){e1.at(TNOD)=(xc[i]<=0.5)?E(l[0],l[2]):E(r[0],r[2]);}}
   for(int i=0;i<ng;i++){V0.at(i)=DX0;V1.at(i)=DX1;}
@@ -112,7 +117,7 @@ int main(){
   for(int i=0;i<ng;i++){m.at(i)=(xc[i]<=0.5)?l[0]*V0[i]:r[0]*V0[i];}
   for(long i=0;i<nkg;i++){u0.at(i)=(x0[i]<=0.5)?l[1]:r[1];u1.at(i)=u0[i];}
   for(int i=0;i<ng;i++){for(int gi=0;gi<T.ngi();gi++){q.at(GPNT)=0.0;}}
-  for(int i=0;i<ng;i++){for(int gi=0;gi<T.ngi();gi++){c.at(GPNT)=sqrt(GAMMA*p[GPNT]/d0[GPNT]);}}
+  for(int i=0;i<ng;i++){for(int gi=0;gi<T.ngi();gi++){c.at(GPNT)=sqrt(GAMMA*p[GPNT]/d0_k[GPNT]);}}
 
 // set thermodynamic node positions at time-0
 
@@ -123,27 +128,47 @@ int main(){
     }
   }
 
-// set time-0 Jacobian
+// set time-0 Jacobians
 
   for(int i=0;i<ng;i++){
-    for(int gi=0;gi<T.ngi();gi++){
-      detJ0.at(GPNT)=0.0;
+
+    for(int gi=0;gi<K.ngi();gi++){
+      detJ0_k.at(GPNT)=0.0;
       for(int k=0;k<K.nloc();k++){
-        detJ0.at(GPNT)+=K.dvalue(k,gi)*x0[KNOD];
+        detJ0_k.at(GPNT)+=K.dvalue(k,gi)*x0[KNOD];
       }
-      if(detJ0.at(GPNT)<0.0){cout<<"-'ve determinant of J0 detected in cell "<<i<<endl;exit(1);}
+      if(detJ0_k.at(GPNT)<0.0){cout<<"-'ve determinant of J0_k detected in cell "<<i<<endl;exit(1);}
     }
+
+    for(int gi=0;gi<T.ngi();gi++){
+      detJ0_t.at(GPNT)=0.0;
+      for(int j=0;j<T.nloc();j++){
+        detJ0_t.at(GPNT)+=T.dvalue(j,gi)*x2[TNOD];
+      }
+      if(detJ0_t.at(GPNT)<0.0){cout<<"-'ve determinant of J0_t detected in cell "<<i<<endl;exit(1);}
+    }
+
   }
 
 // set nodal masses - these should not change with time
 
+  for(long i=0;i<ntg;i++){nodmass_t[i]=0.0;}
+  for(long i=0;i<nkg;i++){nodmass_k[i]=0.0;}
+
   for(int i=0;i<ng;i++){
+
     for(int j=0;j<T.nloc();j++){
-      nodmass[TNOD]=0.0;
       for(int gi=0;gi<T.ngi();gi++){
-        nodmass[TNOD]+=dinit[i]*T.value(j,gi)*detJ0[GPNT]*T.wgt(gi);
+        nodmass_t[TNOD]+=dinit[i]*T.value(j,gi)*detJ0_t[GPNT]*T.wgt(gi);
       }
     }
+
+    for(int k=0;k<K.nloc();k++){
+      for(int gi=0;gi<K.ngi();gi++){
+        nodmass_k[KNOD]+=dinit[i]*K.value(k,gi)*detJ0_k[GPNT]*K.wgt(gi);
+      }
+    }
+
   }
 
 // start the Riemann solvers from initial flux states
@@ -162,14 +187,14 @@ int main(){
 
     for(int i=0;i<ng;i++){
       for(int gi=0;gi<T.ngi();gi++){
-        dt_cfl.at(GPNT)=COURANT*T.wgt(gi)*(DX0/sqrt((c[GPNT]*c[GPNT])+2.0*q[GPNT]/d0[GPNT]));
+        dt_cfl.at(GPNT)=COURANT*T.wgt(gi)*(DX0/sqrt((c[GPNT]*c[GPNT])+2.0*q[GPNT]/d0_k[GPNT]));
       }
     }
 
 // reduce across element and apply a saftey factor
 
-    double dt=DTSFACTOR*(*min_element(dt_cfl.begin(), dt_cfl.end()));
-//    dt=DTSTART;cout<<"DT HARDWIRED !! "<<endl;
+//    double dt=DTSFACTOR*(*min_element(dt_cfl.begin(), dt_cfl.end()));
+    dt=DTSTART;cout<<"DT HARDWIRED !! "<<endl;
 
     cout<<fixed<<setprecision(5)<<"  step "<<step<<" time= "<<time<<" dt= "<<dt;
     cout<<fixed<<setprecision(5)<<" energy (i/k/tot)= "<<ie<<" "<<ke<<" "<<ie+ke<<endl;
@@ -212,15 +237,24 @@ int main(){
 
     for(int i=0;i<ng;i++){V1.at(i)=DX1;if(V1[i]<VTOL){cout<<"-'ve volume detected in cell "<<i<<endl;exit(1);}}
 
-// update Jacobian
+// update Jacobians
 
     for(int i=0;i<ng;i++){
-      for(int gi=0;gi<T.ngi();gi++){
-        detJ.at(GPNT)=0.0;
+
+      for(int gi=0;gi<K.ngi();gi++){
+        detJ_k.at(GPNT)=0.0;
         for(int k=0;k<K.nloc();k++){
-          detJ.at(GPNT)+=K.dvalue(k,gi)*x1[KNOD];
+          detJ_k.at(GPNT)+=K.dvalue(k,gi)*x1[KNOD];
         }
-//        if(detJ.at(GPNT)<0.0){cout<<"-'ve determinant of J detected in cell "<<i<<endl;exit(1);}
+//        if(detJ_k.at(GPNT)<0.0){cout<<"-'ve determinant of Jk detected in cell "<<i<<endl;exit(1);}
+      }
+
+      for(int gi=0;gi<T.ngi();gi++){
+        detJ_t.at(GPNT)=0.0;
+        for(int j=0;j<T.nloc();j++){
+          detJ_t.at(GPNT)+=T.dvalue(j,gi)*x3[TNOD];
+        }
+//        if(detJ_t.at(GPNT)<0.0){cout<<"-'ve determinant of Jt detected in cell "<<i<<endl;exit(1);}
       }
     }
 
@@ -232,10 +266,9 @@ int main(){
         for(int j=0;j<T.nloc();j++){
           double f(0.0);
           for(int gi=0;gi<T.ngi();gi++){
-//            f+=(p[GPNT]+q[GPNT])*K.dvalue(k,gi)*T.value(j,gi)*detJ[gi]*T.wgt(gi)*JI;
-            f+=(p[GPNT]+q[GPNT])*K.dvalue(k,gi)*T.value(j,gi)*T.wgt(gi);
+            f+=(p[GPNT]+q[GPNT])*K.dvalue(k,gi)*T.value(j,gi)*K.wgt(gi);
           }
-          F[KNOD][TNOD]=+f;
+          F[KNOD][TNOD]=f;
         }
       }
     }
@@ -243,109 +276,46 @@ int main(){
 
 // update cell density at the full-step
 
-    for(int i=0;i<ng;i++){for(int gi=0;gi<T.ngi();gi++){d1[GPNT]=dinit[i]*detJ0[GPNT]/detJ[GPNT];}}
+    for(int i=0;i<ng;i++){for(int gi=0;gi<T.ngi();gi++){d1_t[GPNT]=dinit[i]*detJ0_t[GPNT]/detJ_t[GPNT];}}
+    for(int i=0;i<ng;i++){for(int gi=0;gi<K.ngi();gi++){d1_k[GPNT]=dinit[i]*detJ0_k[GPNT]/detJ_k[GPNT];}}
 
-// set nodal masses - these should not change with time
+// set nodal volumes
 
-  for(int i=0;i<ng;i++){
-    for(int j=0;j<T.nloc();j++){
-      nodmass[TNOD]=0.0;
-      for(int gi=0;gi<T.ngi();gi++){
-//        nodmass[TNOD]+=d1[i]*T.value(j,gi)*detJ[GPNT]*T.wgt(gi);
-        nodmass[TNOD]+=T.value(j,gi)*detJ[GPNT]*T.wgt(gi); // why isn't density in here ? Looks more like a vol. P2Q1 energy correct height but noisy
-//        nodmass[TNOD]+=d1[GPNT]*detJ[GPNT]*T.wgt(gi); // is this the real nodal mass ?                         P2Q1 energy too high but not noisy
-//        nodmass[TNOD]+=T.value(j,gi)*d1[GPNT]*detJ[GPNT]*T.wgt(gi); // is this the real nodal mass ?                         P2Q1 energy too high but not noisy
-      }
-    }
-  }
-
-// debug
-//    for(int i=0;i<ng;i++){for(int gi=0;gi<T.ngi();gi++){d1.at(GPNT)=R2.density(GPNT);}}
-// debug
-
-// update Jacobian for energy solve
+    for(long i=0;i<ntg;i++){nodvol_t[i]=0.0;}
+    for(long i=0;i<nkg;i++){nodvol_k[i]=0.0;}
 
     for(int i=0;i<ng;i++){
-      for(int gi=0;gi<T.ngi();gi++){
-        detJ.at(GPNT)=0.0;
-        for(int j=0;j<T.nloc();j++){
-          detJ.at(GPNT)+=T.dvalue(j,gi)*x3[TNOD];
+      for(int j=0;j<T.nloc();j++){
+        for(int gi=0;gi<T.ngi();gi++){
+          nodvol_t[TNOD]+=T.value(j,gi)*detJ_t[GPNT]*T.wgt(gi);
         }
-//        if(detJ.at(GPNT)<0.0){cout<<"-'ve determinant of J detected in cell "<<i<<endl;exit(1);}
+      }
+      for(int k=0;k<K.nloc();k++){
+        for(int gi=0;gi<K.ngi();gi++){
+          nodvol_k[KNOD]+=K.value(k,gi)*detJ_k[GPNT]*K.wgt(gi);
+        }
       }
     }
+
+// debug
+//    for(int i=0;i<ng;i++){for(int gi=0;gi<T.ngi();gi++){d1_t.at(GPNT)=R2.density(GPNT);d1_k.at(GPNT)=R2.density(GPNT);}}
+// debug
 
 // assemble finite element energy field on discontinuous thermodynamic grid
 // for( struct {int i; double j;} v = {0, 3.0}; v.i < 10; v.i++, v.j+=0.1)
 
     {Matrix A(T.nloc());double b[T.nloc()],x[T.nloc()];
-    for(int i=0;i<ng;i++){int k(0);
-      for(int iloc=0;iloc<T.nloc();iloc++,k++){
-        b[iloc]=0.0;int j(0);
-        for(int jloc=0;jloc<T.nloc();jloc++,j++){
-          b[iloc]+=FT[TNOD][KNOD]*u1[KNOD];
+    for(int i=0;i<ng;i++){
+      for(int j=0;j<T.nloc();j++){
+        b[j]=0.0;for(int k=0;k<K.nloc();k++){b[j]+=FT[TNOD][KNOD]*u1[KNOD];}
+        for(int k=0;k<T.nloc();k++){
           double nn(0.0); // DG mass matrix
-           for(int gi=0;gi<T.ngi();gi++){
-             nn+=d1[GPNT]*T.value(iloc,gi)*T.value(jloc,gi)*detJ[GPNT]*T.wgt(gi);
-           }
-           A.write(iloc,jloc,nn);
-        }
-      }
-
-// debug
-//      double gradu[T.ngi()];
-//      for(int gi=0;gi<T.ngi();gi++){
-//        gradu[gi]=0.0;
-//        for(int iloc=0;iloc<K.nloc();iloc++){
-//          gradu[gi]+=K.dvalue(iloc,gi)*u1[i*(K.nloc()-1)+iloc];
-//        }
-//      }
-
-      double P[T.nloc()][K.nloc()];
-      for(int j=0;j<T.nloc();j++){
-        for(int k=0;k<K.nloc();k++){
-          P[j][k]=0.0;
           for(int gi=0;gi<T.ngi();gi++){
-            P[j][k]+=T.value(j,gi)*K.dvalue(k,gi)*T.wgt(gi);
+            nn+=d1_k[GPNT]*T.value(j,gi)*T.value(k,gi)*detJ_t[GPNT]*T.wgt(gi);
           }
+          A.write(j,k,nn);
         }
       }
-
-      double ujloc[T.nloc()];
-      for(int j=0;j<T.nloc();j++){
-        ujloc[j]=0.0;
-        for(int k=0;k<K.nloc();k++){
-          ujloc[j]+=P[j][k]*u1[KNOD];
-        }
-      }
-
-      double gradu[T.ngi()];
-      for(int gi=0;gi<T.ngi();gi++){
-        gradu[gi]=0.0;
-        for(int j=0;j<T.nloc();j++){
-          gradu[gi]+=T.value(j,gi)*ujloc[j];
-        }
-      }
-
-
-      for(int iloc=0;iloc<T.nloc();iloc++){
-        b[iloc]=0.0;
-        for(int gi=0;gi<T.ngi();gi++){
-          b[iloc]+=(p[GPNT]+q[GPNT])*gradu[gi]*T.value(iloc,gi)*detJ[GPNT]*T.wgt(gi);
-        }
-      }
-
-//      for(int j=0;j<T.nloc();j++){
-//        b[j]=0.0;
-//        for(int k=0;k<K.nloc();k++){
-//          for(int gi=0;gi<T.ngi();gi++){
-//            b[j]+=(p[GPNT]+q[GPNT])*K.dvalue(k,gi)*T.value(j,gi)*detJ[GPNT]*T.wgt(gi)*u1[KNOD];
-//          }
-//        }
-//      }
-
-
-// debug
 
 // solve local system
 
@@ -353,8 +323,7 @@ int main(){
 
 // advance the solution
 
-      for(int j=0;j<T.nloc();j++){e1.at(TNOD)=max(ECUT,e0[TNOD]-x[j]*dt/nodmass[TNOD]);}
-//      for(int j=0;j<T.nloc();j++){e1.at(TNOD)=max(ECUT,e0[TNOD]-x[j]*dt);}
+      for(int j=0;j<T.nloc();j++){e1.at(TNOD)=max(ECUT,e0[TNOD]-x[j]*dt);}
     }}
 
 // debug
@@ -373,7 +342,7 @@ int main(){
         for(int j=0;j<T.nloc();j++){
           egi+=T.value(j,gi)*e1[TNOD];
         }
-        p.at(GPNT)=P(d1[GPNT],egi); // this is the EOS call
+        p.at(GPNT)=P(d1_k[GPNT],egi); // this is the EOS call
       }
     }
 
@@ -383,31 +352,19 @@ int main(){
 
 // update sound speed
 
-    for(int i=0;i<ng;i++){for(int gi=0;gi<T.ngi();gi++){c.at(GPNT)=sqrt(GAMMA*p[GPNT]/d1[GPNT]);}}
+    for(int i=0;i<ng;i++){for(int gi=0;gi<T.ngi();gi++){c.at(GPNT)=sqrt(GAMMA*p[GPNT]/d1_k[GPNT]);}}
 
 // bulk q
 
     for(int i=0;i<ng;i++){
       double l(DX1);
       for(int gi=0;gi<T.ngi();gi++){
-        double divu((d0[GPNT]-d1[GPNT])/(d1[GPNT]*dt));
+        double divu((d0_k[GPNT]-d1_k[GPNT])/(d1_k[GPNT]*dt));
         if(divu<0.0){
-          q.at(GPNT)=d0[GPNT]*l*divu*((cq*l*divu)-cl*c[GPNT]);
+          q.at(GPNT)=d0_k[GPNT]*l*divu*((cq*l*divu)-cl*c[GPNT]);
         }else{
           q.at(GPNT)=0.0; // turn off q as cell divergence indicates expansion
         }
-      }
-    }
-
-// update Jacobian for momentum solve
-
-    for(int i=0;i<ng;i++){
-      for(int gi=0;gi<T.ngi();gi++){
-        detJ.at(GPNT)=0.0;
-        for(int k=0;k<K.nloc();k++){
-          detJ.at(GPNT)+=K.dvalue(k,gi)*x1[KNOD];
-        }
-//        if(detJ.at(GPNT)<0.0){cout<<"-'ve determinant of J detected in cell "<<i<<endl;exit(1);}
       }
     }
 
@@ -456,7 +413,7 @@ int main(){
         for(int jloc=0;jloc<K.nloc();jloc++){
           double nn(0.0); // mass matrix
           for(int gi=0;gi<K.ngi();gi++){
-            nn+=d1[GPNT]*K.value(iloc,gi)*K.value(jloc,gi)*detJ[GPNT]*K.wgt(gi);
+            nn+=d1_k[GPNT]*K.value(iloc,gi)*K.value(jloc,gi)*detJ_k[GPNT]*K.wgt(gi);
           }
           A.add(ROW,COL,nn);
         }
@@ -495,7 +452,7 @@ int main(){
     }
     for(long i=0;i<ntg;i++){f2<<x3[i]<<" "<<e1[i]<<endl;}
     for(long i=0;i<nkg;i++){f3<<x1[i]<<" "<<u1[i]<<endl;}
-    for(int i=0;i<ng;i++){for(int gi=0;gi<T.ngi();gi++){double xgi(0.0);XGI;f4<<xgi<<" "<<d1[GPNT]<<" "<<p[GPNT]<<endl;}}
+    for(int i=0;i<ng;i++){for(int gi=0;gi<T.ngi();gi++){double xgi(0.0);XGI;f4<<xgi<<" "<<d1_k[GPNT]<<" "<<p[GPNT]<<endl;}}
 //    cout<<"NODE POS: "<<time<<" "<<x1[(ng/2)*(K.nloc()-1)]<<" "<<x1[(ng/2)*(K.nloc()-1)+1]<<endl;
     for(int i=0;i<ng;i++){
       int k;
@@ -520,7 +477,8 @@ int main(){
     for(int i=0;i<ntg;i++){e0.at(i)=e1[i];}
     for(int i=0;i<ntg;i++){x2.at(i)=x3[i];}
     for(int i=0;i<ng;i++){V0.at(i)=V1[i];}
-    for(int i=0;i<ng*T.ngi();i++){d0.at(i)=d1[i];}
+    for(int i=0;i<ng*T.ngi();i++){d0_t.at(i)=d1_t[i];}
+    for(int i=0;i<ng*K.ngi();i++){d0_k.at(i)=d1_k[i];}
 
 // debug
 //  cout<<"debug stop."<<endl;
@@ -545,3 +503,114 @@ double E(double d,double p){return p/((GAMMA-1.0)*d);}
 // empty a vector
 
 void vempty(vector<double>&v){vector<double> e;v.swap(e);return;}
+
+// debug - check nodal quantities, insert this soon after nodmass_k, nodvol_k etc
+//
+//  {
+//  cout<<endl;
+//  cout<<"nodal mass check:"<<endl;
+//  double nmassk[K.nloc()],nmasst[T.nloc()];
+//  double meshmassk(0.0),meshmasst(0.0);
+//
+//  for(int i=0;i<ng;i++){
+//
+//    double sum_k(0.0),sum_t(0.0);
+//
+//    for(int k=0;k<K.nloc();k++){
+//      nmassk[k]=0.0;
+//      for(int gi=0;gi<K.ngi();gi++){
+//        nmassk[k]+=d1_k[GPNT]*K.value(k,gi)*detJ_k[GPNT]*K.wgt(gi);
+//      }
+//    }
+//    for(int j=0;j<T.nloc();j++){
+//      nmasst[j]=0.0;
+//      for(int gi=0;gi<T.ngi();gi++){
+//        nmasst[j]+=d1_t[GPNT]*T.value(j,gi)*detJ_t[GPNT]*T.wgt(gi);
+//      }
+//    }
+//
+//    if(i<10){
+//      cout<<i<<"  m[i]= "<<m[i]<<" K: ";
+//    }else{
+//      cout<<i<<" m[i]= "<<m[i]<<" K: ";
+//    }
+//
+//    for(int k=0;k<K.nloc();k++){sum_k+=nodmass_k[KNOD];cout<<nodmass_k[KNOD]<<" ";}
+////    for(int k=0;k<K.nloc();k++){sum_k+=nmassk[k];cout<<nmassk[k]<<" ";}
+//    cout<<" T: ";
+//
+//    for(int j=0;j<T.nloc();j++){sum_t+=nodmass_t[TNOD];cout<<nodmass_t[TNOD]<<" ";}
+////    for(int j=0;j<T.nloc();j++){sum_t+=nmasst[j];cout<<nmasst[j]<<" ";}
+//    cout<<" sum_k= "<<sum_k<<" sum_t= "<<sum_t<<endl;
+//
+//  }
+//
+//  for(long i=0;i<ntg;i++){meshmasst+=nodmass_t[i];}
+//  for(long i=0;i<nkg;i++){meshmassk+=nodmass_k[i];}
+//
+//  cout<<"meshmassk= "<<meshmassk<<" meshmasst= "<<meshmasst<<endl;
+//
+//  cout<<endl;
+//  cout<<"nodal volume check:"<<endl;
+//  double nvolk[K.nloc()],nvolt[T.nloc()];
+//  double meshvolk(0.0),meshvolt(0.0);
+//
+//  for(int i=0;i<ng;i++){
+//
+//    double sum_k(0.0),sum_t(0.0);
+//
+//    for(int k=0;k<K.nloc();k++){
+//      nvolk[k]=0.0;
+//      for(int gi=0;gi<K.ngi();gi++){
+//        nvolk[k]+=K.value(k,gi)*detJ_k[GPNT]*K.wgt(gi);
+//      }
+//    }
+//    for(int j=0;j<T.nloc();j++){
+//      nvolt[j]=0.0;
+//      for(int gi=0;gi<T.ngi();gi++){
+//        nvolt[j]+=T.value(j,gi)*detJ_t[GPNT]*T.wgt(gi);
+//      }
+//    }
+//
+//    if(i<10){
+//      cout<<i<<"  V1[i]= "<<V1[i]<<" K: ";
+//    }else{
+//      cout<<i<<" V1[i]= "<<V1[i]<<" K: ";
+//    }
+//
+//    for(int k=0;k<K.nloc();k++){sum_k+=nodvol_k[KNOD];cout<<nodvol_k[KNOD]<<" ";}
+////    for(int k=0;k<K.nloc();k++){sum_k+=nvolk[k];cout<<nvolk[k]<<" ";}
+//    cout<<" T: ";
+//
+//    for(int j=0;j<T.nloc();j++){sum_t+=nodvol_t[TNOD];cout<<nodvol_t[TNOD]<<" ";}
+////    for(int j=0;j<T.nloc();j++){sum_t+=nvolt[j];cout<<nvolt[j]<<" ";}
+//    cout<<" sum_k= "<<sum_k<<" sum_t= "<<sum_t<<endl;
+//
+//  }
+//
+//  for(long i=0;i<ntg;i++){meshvolt+=nodvol_t[i];}
+//  for(long i=0;i<nkg;i++){meshvolk+=nodvol_k[i];}
+//
+//  cout<<"meshvolk= "<<meshvolk<<" meshvolt= "<<meshvolt<<endl;
+//
+//  cout<<endl;
+//
+//  cout<<"x1[] nodvol_k"<<endl;
+//  for(long i=0;i<nkg;i++){cout<<x1[i]<<" "<<nodvol_k[i]<<endl;}
+//  cout<<endl;
+//
+//  cout<<"x3[] nodvol_t"<<endl;
+//  for(long i=0;i<ntg;i++){cout<<x3[i]<<" "<<nodvol_t[i]<<endl;}
+//  cout<<endl;
+//
+//  cout<<"x1[] nodmass_k"<<endl;
+//  for(long i=0;i<nkg;i++){cout<<x1[i]<<" "<<nodmass_k[i]<<endl;}
+//  cout<<endl;
+//
+//  cout<<"x3[] nodmass_t"<<endl;
+//  for(long i=0;i<ntg;i++){cout<<x3[i]<<" "<<nodmass_t[i]<<endl;}
+//  cout<<endl;
+//
+//  }
+
+// debug - check nodal quantities
