2f392859fce399a0030791b3712bb2f40d1ceff5
smerton <simonmerton@hotmail.co.uk>
2022-01-15 01:19:24 +0000

    Add code to generate a shape function in global, physical coordinates to the shape class.

--- shape.cpp
+++ shape.cpp
@@ -7,10 +7,13 @@
 #include "shape.h"
 #include "quadrature.h"
 #include "polynomial.h"
+#include "matrix.h"  // matrix operations
+
+double delta(int i,int j); // kronecker delta function
 
 using namespace std;
 
-// constructor to instantiate a new shape object and all its attributes
+// constructor to instantiate a new shape object and all its attributes in local coordinates
 
 Shape::Shape(int n){
 
@@ -95,6 +98,123 @@ Shape::Shape(int n){
 
 }
 
+// constructor to instantiate a new shape object and all its attributes in global coordinates
+
+Shape::Shape(int n,vector<vector<double> > x){
+
+// this is only coded for bilinear case
+
+  if(n!=1){
+    cout<<"Shape::Shape(): Only bilinear shapes can be constructed in global coordinates, n= "<<n<<endl;
+    cout<<"Shape::Shape(): Stopping"<<endl;
+    exit(1);
+  }
+
+// set the polyhedral order
+
+  morder=n;
+
+// set the number of dimensions
+
+  mndims=2; // shouldn't we be getting this from the mesh class ?
+
+// set number of local nodes
+
+  mnloc=(this->order()+1)*(this->order()+1);
+
+// set number of surface nodes on a face
+
+  msloc=this->order()+1;
+
+// set number of integration points adequate for the shape
+
+  mngi=(this->order()+1)*(this->order()+1);
+
+// set number of faces
+
+  mnfaces=4;
+
+// set up reflection across each face
+
+  vector<int> reflections[this->nfaces()];
+
+  reflections[0].push_back(2);
+  reflections[1].push_back(1);
+  reflections[2].push_back(2);
+  reflections[3].push_back(3);
+
+  for(int iface=0;iface<this->nfaces();iface++){
+    mreflect.push_back(reflections[iface]);
+  }
+
+// node positions
+
+  for(int j=0,k=0;j<sloc();j++){
+    for(int i=0;i<sloc();i++,k++){
+      mpos[0].push_back(i);
+      mpos[1].push_back(j);
+    }
+  }
+
+  cout<<"Setting up a shape in global coordinates:"<<endl;
+  for(int iloc=0;iloc<mnloc;iloc++){
+    cout<<" node "<<iloc<<" x,y= "<<x.at(0).at(iloc)<<","<<x.at(1).at(iloc)<<endl;
+  }
+
+// assemble a matrix equation for the coefficients
+
+  Matrix A(mnloc),AI(mnloc);
+
+  for(int iloc=0;iloc<mnloc;iloc++){
+    A.write(iloc,0,1.0);
+    A.write(iloc,1,x.at(0).at(iloc));
+    A.write(iloc,2,x.at(1).at(iloc));
+    A.write(iloc,3,x.at(0).at(iloc)*x.at(1).at(iloc));
+  }
+
+// check for singularities which are expected if x1=x3 and y1=y4
+
+//  if(A.singular()){
+//    cout<<"Shape::Shape(): Element has a singularity, unable to construct."<<endl;
+//    cout<<"Shape::Shape(): Stopping."<<endl;
+//    exit(1);
+//  }
+
+// solve to get the coefficients
+
+  for(int iloc=0;iloc<mnloc;iloc++){
+
+// rhs
+
+    double b[mnloc];
+
+    for(int jloc=0;jloc<mnloc;jloc++){
+      b[jloc]=delta(iloc,jloc);
+    }
+
+// inverse
+
+    AI.inverse2(&A); // lapack drivers dgetrf_ and dgetri_
+
+// solve for the coefficient for shape function iloc
+
+    vector<double> coeffs_iloc;
+    for(int jloc=0;jloc<mnloc;jloc++){
+      double xsoln(0.0);
+      for(int kloc=0;kloc<mnloc;kloc++){
+        xsoln+=AI.read(jloc,kloc)*b[kloc];
+      }
+      coeffs_iloc.push_back(xsoln);
+    }
+
+// store coefficients for shape function iloc
+
+    mcoeff.push_back(coeffs_iloc);
+
+  }
+
+}
+
 // destructor function to release storage associated with a Shape class object
 
 Shape::~Shape(){}
@@ -136,8 +256,12 @@ int Shape::pos(int idim,int iloc) const {return mpos[idim][iloc];} // node posit
 
 double Shape::wgt(int gi) const {return mwgt[gi];} // quadrature weight of integration point gi
 double Shape::value(int i,int gi) const {return mvalue[i][gi];} // shape i value at Gauss point gi
-double Shape::value(int i,double x,double y) const {Polynomial P1(order(),pos(0,i),-1.0,1.0),P2(order(),pos(1,i),-1.0,1.0);return P1.value(x)*P2.value(y);} // shape i value at coordinate x,y
+double Shape::value(int i,double u,double v) const {Polynomial P1(order(),pos(0,i),-1.0,1.0),P2(order(),pos(1,i),-1.0,1.0);return P1.value(u)*P2.value(v);} // shape i value at coordinate x,y
+double Shape::value(int i,vector<double> x) const {return(mcoeff.at(i).at(0)+mcoeff.at(i).at(1)*x.at(0)+mcoeff.at(i).at(2)*x.at(1)+mcoeff.at(i).at(3)*x.at(0)*x.at(1));}
 double Shape::dvalue(int idim,int i,int gi) const {return mdvalue[idim][i][gi];} // derivative i value at Gauss point gi
-double Shape::dvalue(int idim,int i,double x,double y){Polynomial P1(order(),pos(0,i),-1.0,1.0),P2(order(),pos(1,i),-1.0,1.0);double dval[2];dval[0]=P1.dvalue(x)*P2.value(y);dval[1]=P1.value(x)*P2.dvalue(y);return dval[idim];
+double Shape::dvalue(int idim,int i,double u,double v) const {Polynomial P1(order(),pos(0,i),-1.0,1.0),P2(order(),pos(1,i),-1.0,1.0);double dval[2];dval[0]=P1.dvalue(u)*P2.value(v);dval[1]=P1.value(u)*P2.dvalue(v);return dval[idim];}// shape i derivative value at coordinate x
+double Shape::dvalue(int idim,int i,vector<double> x) const {double dx(mcoeff.at(i).at(1)+mcoeff.at(i).at(3)*x.at(1)),dy(mcoeff.at(i).at(2)+mcoeff.at(i).at(3)*x.at(0));return((idim==0)?dx:dy);} // shape i derivative value at global coordinate x
 
-} // shape i derivative value at coordinate x
+// kronecker delta function
+
+double delta(int i,int j){return (i==j)?1.0:0.0;}
--- shape.h
+++ shape.h
@@ -10,29 +10,33 @@ class Shape{
 
   public:
 
-    Shape(int n);                                       // constructor function for a new shape of order n
-    ~Shape();                                           // destructor to release class storage
+    Shape(int n);                                                  // constructor function for a new shape of order n
+    Shape(int n,vector<vector<double> > x);                        // constructor function for a new shape of order n in global coordinate x
+
+    ~Shape();                                                      // destructor to release class storage
 
 // accessor functions to member data
 
-    int order() const;                                  // returns the polyhedral order of the shape
-    int ndims() const;                                  // returns the number of dimensions of the shape
-    int nloc() const;                                   // returns number of local nodes
-    int sloc() const;                                   // returns number of nodes on the surface
-    int ngi() const;                                    // number of Gauss integration points
-    int nfaces() const;                                 // number of element faces
-    int reflect(int iloc,int iface) const;              // refelct iloc across face iface
-    int pos(int idim,int iloc) const;                   // node number in dimension idim
+    int order() const;                                             // returns the polyhedral order of the shape
+    int ndims() const;                                             // returns the number of dimensions of the shape
+    int nloc() const;                                              // returns number of local nodes
+    int sloc() const;                                              // returns number of nodes on the surface
+    int ngi() const;                                               // number of Gauss integration points
+    int nfaces() const;                                            // number of element faces
+    int reflect(int iloc,int iface) const;                         // refelct iloc across face iface
+    int pos(int idim,int iloc) const;                              // node number in dimension idim
 
-    double wgt(int gi) const;                           // quadrature weight of integration point gi
-    double value(int i,int gi) const;                   // shape i value at Gauss point gi
-    double dvalue(int idim, int i,int gi) const;        // shape i derivative idim at Gauss point gi
-    double value(int i,double x,double y) const;        // shape i value at local coordinate x,y
-    double dvalue(int idim, int i,double x, double y);  // shape i derivative idim at local coordinate x,y
+    double wgt(int gi) const;                                      // quadrature weight of integration point gi
+    double value(int i,int gi) const;                              // shape i value at Gauss point gi
+    double dvalue(int idim,int i,int gi) const;                    // shape i derivative idim at Gauss point gi
+    double value(int i,double u,double v) const;                   // shape i value at local coordinates u,v
+    double value(int i,vector<double> x) const;                    // shape i value at global coordinate x
+    double dvalue(int idim,int i,double u,double v) const;         // shape i derivative idim at local coordinates u,v
+    double dvalue(int idim,int i,vector<double> x) const;          // shape i derivative idim at global coordinate x
 
 // accessor function to prolongation operator
 
-    void prolongate(double*u,double*v,int p);           // prolongation operator to map vector u[] to an order p element
+    void prolongate(double*u,double*v,int p);                      // prolongation operator to map vector u[] to an order p element
 
   private:
 
@@ -51,5 +55,6 @@ class Shape{
     vector<double> mwgt;                                // quadrature weights
     vector<vector<double> > mvalue;                     // shape values
     vector<vector<double > > mdvalue[3];                // derivative values
+    vector<vector<double> > mcoeff;                     // coefficients of teh polynomial
 
 };
